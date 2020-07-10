cd8e0b0385a2bd6f2fbd7d227fbeb8f136d9cd24
CluelessWizard <tremmel.david@hotmail.com>
2020-07-10 15:36:54 +0200

    Functional version with images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstDieResult, setFirstDieResult] = useState(1);
+  const [secondDieResult, setSecondDieResult] = useState(6);
+  const [previous, setPrevious] = useState("");
+  const firstDieImage = require(`./assets/${firstDieResult}.png`);
+  const secondDieImage = require(`./assets/${secondDieResult}.png`);
+
+  function rollDice() {
+    setPrevious(firstDieResult + secondDieResult);
+    setFirstDieResult(Math.floor(Math.random() * 6) + 1);
+    setSecondDieResult(Math.floor(Math.random() * 6) + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ display: "flex", margin: 30 }}>
+          <img src={firstDieImage} className="dice" alt="First" />
+          <img src={secondDieImage} className="dice" alt="Second" />
+        </div>
+        <span>{firstDieResult + secondDieResult}</span>
+        <button className="button" onClick={rollDice}>
+          Roll the dice
+        </button>
+        <div className="history">Previous roll: {previous}</div>
       </header>
     </div>
   );
